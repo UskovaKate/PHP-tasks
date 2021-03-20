3bdace562c16b2fc073f478048dc5f1ec6c7780c
Uskova Ekaterina <55867937+UskovaKate@users.noreply.github.com>
2021-03-21 01:23:14 +0500

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,7 @@
+<?php
+//Даны действительные числа А, В, С. Найти максимальное и минимальное из этих чисел.
+$number=array("A"=>35,"B"=>45,"C"=>56);
+echo "A= ".$number["A"].", B= ".$number["B"].", C= ".$number["C"]."<br><br>"; //Outputs A= 35, B= 45, C= 56
+echo "<b>Max=</b> ".max($number)."<br>"; //Outputs 56
+echo "<b>Min=</b> ".min($number); //Outputs 35
+ ?>
