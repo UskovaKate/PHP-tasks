07438be54e8be7a1e835c6e840205e053fdb3fb9
Uskova Ekaterina <55867937+UskovaKate@users.noreply.github.com>
2021-05-18 16:24:46 +0500

    Update 12.php

--- 12.php
+++ 12.php
@@ -1,7 +1,3 @@
-/* Project Name: 12 task
-    Create Date	: 21.03.21
-    Autor: (C) Uskova Ekaterina, Chelyabinsk, 2021*/
-    
 <?php
 //Дано вещественное число. Определить, какое это число: положительное, отрицательное, ноль.
 $x=rand(-1000,1000);
