0ed364fdaf469e6811c2d2e74119c992df858689
Uskova Ekaterina <55867937+UskovaKate@users.noreply.github.com>
2021-05-18 16:22:34 +0500

    Update 2.php

--- 2.php
+++ 2.php
@@ -1,7 +1,3 @@
-/* Project Name: 2 task
-    Create Date	: 20.03.21
-    Autor: (C) Uskova Ekaterina, Chelyabinsk, 2021*/
-
 <?php
 //Даны действительные числа А, В, С. Найти максимальное и минимальное из этих чисел.
 $number=array("A"=>35,"B"=>45,"C"=>56);
